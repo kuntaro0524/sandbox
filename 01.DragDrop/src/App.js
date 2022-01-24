e6911d5eedad6cda25e5a00d8de6cc87796855d3
Kunio Hirata <kuntaro@robo05.spring8.or.jp>
2022-01-24 18:35:02 +0900

    ドラッグしてドロップしたらアイコン的なものの順番が変わるようになったぞ！

--- 01.DragDrop/src/App.js
+++ 01.DragDrop/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import { useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState(["koko0", "koko1", "koko2"]);
   const onDragEnd =(result)=>{
-    console.log(result);
+    const new_items = [...items];
+    // 動かしたところを一個削除する
+    const remove  = new_items.splice(result.source.index,1);
+    new_items.splice(result.destination.index, 0, remove[0])
+    console.log(new_items);
+    setItems(new_items);
   }
   return (<div className="dragDropArea">
     {/* ドラッグが終わった瞬間にプロパティを作動させる→onDragEndに関数を割り当てる */}
@@ -13,17 +20,17 @@ function App() {
         {(provided) => (
           // ライブラリのお作法的に利用する
           // わからなくなったらdocumentを調べるのが良いと思います
-          // {/* 何番目のドラッガブルか？ */}
-          // {/* 掴んで移動させたいとき: dragHandleProps を追加すること */}
+          // 何番目のドラッガブルか？
+          // 掴んで移動させたいとき: dragHandleProps を追加すること 
           <div {...provided.droppableProps} ref={provided.innerRef}>
           <Draggable draggableId="item0" index={0}>{(provided)=><div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className="item"> 
-          item0</div>}
+          {items[0]}</div>}
           </Draggable>
           <Draggable  draggableId="item1" index={1}>{(provided)=><div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className="item"> 
-          item1</div>}
+          {items[1]}</div>}
           </Draggable>
           <Draggable  draggableId="item2" index={2}>{(provided)=><div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className="item"> 
-          item2</div>}
+          {items[2]}</div>}
           </Draggable>
           {/* お作法として書かないとWarningがでる */}
           {provided.placeholder}
