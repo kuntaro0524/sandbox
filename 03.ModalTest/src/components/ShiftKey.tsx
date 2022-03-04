4e5bdbfc10eafb00d81028b5ef706631d59dc5f3
Kunio Hirata <kuntaro@kundev.spring8.or.jp>
2022-03-04 10:19:30 +0900

    何ボタンが押しているかの仕組みをコピペ 参考にしたページ: https://zenn.dev/naoki/articles/b45f0d1b79808b2d307e

--- 03.ModalTest/src/components/ShiftKey.tsx
+++ 03.ModalTest/src/components/ShiftKey.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useCallback } from "react";
 
 export const ShiftKey = () => {
   const escFunction = useCallback((event) => {
-    console.log(event.keyCode);
+    console.log("今押しているのは" + event.keyCode);
 
     if (event.keyCode === 27) {
       // キーコードを判定して何かする。
